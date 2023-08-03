892a70d5641382c2ae37d92460c44ff3ab8ac18d
Shamil <shamilbedru47@gmail.com>
2023-08-03 11:48:32 +0300

    Update main.dart

--- On-boarding/Circular Counter/circular_counter/lib/main.dart
+++ On-boarding/Circular Counter/circular_counter/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Circular Counter',
       theme: ThemeData.dark(
         useMaterial3: true,
       ),
