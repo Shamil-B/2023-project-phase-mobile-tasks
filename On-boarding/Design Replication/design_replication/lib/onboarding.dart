818bffe1f2ee1beab0e503dee6e6329ace65cebd
shamil-b <shamilbedru47@gmail.com>
2023-08-03 15:52:47 +0300

    onboarding screen done

---
+++ On-boarding/Design Replication/design_replication/lib/onboarding.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(children: [
+          Container(
+            margin: const EdgeInsets.only(top: 120, bottom: 50),
+            padding: const EdgeInsets.symmetric(vertical: 150, horizontal: 100),
+            child: const Image(
+              image: AssetImage("assets/images/bg1.png"),
+            ),
+          ),
+          ElevatedButton(
+            style: ButtonStyle(
+                shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                padding: const MaterialStatePropertyAll(
+                    EdgeInsets.symmetric(vertical: 15, horizontal: 30)),
+                backgroundColor:
+                    const MaterialStatePropertyAll(Color(0xFF0C8CE9))),
+            onPressed: () {},
+            child: const Text(
+              "Get Started",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w400),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
