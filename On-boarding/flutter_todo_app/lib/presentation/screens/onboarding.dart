f51540c2783fb231ce75fad00a9cf2fc76b5fd8b
shamil-b <shamilbedru47@gmail.com>
2023-08-07 13:22:23 +0300

    Testing done for to do app

---
+++ On-boarding/flutter_todo_app/lib/presentation/screens/onboarding.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import "home_screen.dart";
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(children: [
+          Container(
+            margin: const EdgeInsets.only(top: 120, bottom: 50),
+            padding: const EdgeInsets.symmetric(vertical: 150, horizontal: 100),
+            child: const Image(
+              image: AssetImage("assets/images/bg1.png"),
+            ),
+          ),
+          ElevatedButton(
+            style: ButtonStyle(
+                shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                padding: const MaterialStatePropertyAll(
+                    EdgeInsets.symmetric(vertical: 15, horizontal: 30)),
+                backgroundColor:
+                    const MaterialStatePropertyAll(Color(0xFF0C8CE9))),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()));
+            },
+            child: const Text(
+              "Get Started",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w400),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
---
+++ On-boarding/flutter_todo_app/test/widget_test.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:todo_app/main.dart';
+import "package:todo_app/presentation/screens/home_screen.dart";
+import "package:todo_app/presentation/screens/onboarding.dart";
+import "package:todo_app/presentation/widgets/custom_dialog_newtodo.dart";
+// This is a basic Flutter widget test.
+
+void main() {
+  // Testing the task creation
+  testWidgets('Create a task', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our OnboardingPage is rendered.
+    expect(find.byType(OnboardingPage), findsOneWidget);
+
+    // Tap the 'Get Started' button.
+    await tester.tap(find.text('Get Started'));
+    await tester.pump();
+
+    // Verify that our HomeScreen is rendered.
+    expect(find.byType(HomeScreen), findsOneWidget);
+
+    // Tap the 'Add Task' button.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our CustomdialogNewTodo (task adder widget) is rendered.
+    expect(find.byType(CustomdialogNewTodo), findsOneWidget);
+
+    // Enter 'Test Task' into the TextField.
+    await tester.enterText(find.byType(TextField), 'Test Task');
+    await tester.pump();
+
+    // Tap the 'Add' button.
+    await tester.tap(find.text('Create'));
+    await tester.pump();
+
+    // Verify that our task is rendered.
+    expect(find.text('Test Task'), findsOneWidget);
+  });
+
+  // Testing task listing
+  testWidgets('List tasks', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our OnboardingPage is rendered.
+    expect(find.byType(OnboardingPage), findsOneWidget);
+
+    // Tap the 'Get Started' button.
+    await tester.tap(find.text('Get Started'));
+    await tester.pump();
+
+    // Verify that our HomeScreen is rendered.
+    expect(find.byType(HomeScreen), findsOneWidget);
+
+    // Tap the 'Create Task' button.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our CustomdialogNewTodo (task adder widget) is rendered.
+    expect(find.byType(CustomdialogNewTodo), findsOneWidget);
+
+    // Enter 'Test Task' into the TextField.
+    await tester.enterText(find.byType(TextField), 'Test Task');
+    await tester.pump();
+
+    // Tap the 'Create' button.
+    await tester.tap(find.text('Create'));
+    await tester.pump();
+
+    // Verify that our task is rendered.
+    expect(find.text('Test Task'), findsOneWidget);
+  });
+
+  testWidgets('Navigate from onboarding.', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our OnboardingPage is rendered.
+    expect(find.byType(OnboardingPage), findsOneWidget);
+
+    // Tap the 'Get Started' button.
+    await tester.tap(find.text('Get Started'));
+    await tester.pump();
+
+    // Verify that our HomeScreen is rendered.
+    expect(find.byType(HomeScreen), findsOneWidget);
+  });
+}
