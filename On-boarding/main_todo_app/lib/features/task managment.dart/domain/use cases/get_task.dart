2325129cd92e796b4ef98a94e95551cf2e30110f
shamil-b <shamilbedru47@gmail.com>
2023-08-09 10:46:48 +0300

    added use cases

--- On-boarding/main_todo_app/lib/features/task managment.dart/domain/use cases/get_task.dart
+++ On-boarding/main_todo_app/lib/features/task managment.dart/domain/use cases/get_task.dart
@@ -1,15 +1,22 @@
 import 'package:dartz/dartz.dart';
+import 'package:main_todo_app/core/error/unknown_failure.dart';
+import 'package:main_todo_app/features/task%20managment.dart/data/repository/task_repository_implementation.dart';
 import 'package:main_todo_app/features/task%20managment.dart/domain/entities/task.dart';
 
 import '../../../../core/error/failure.dart';
 import '../repository/task_repository.dart';
 
 class GetTask {
-  final TaskRepository repository;
+  final TaskRepositoryImpl repository;
 
   GetTask(this.repository);
 
-  ToDoTask call(int id) {
-    return repository.getTask(id);
+  Either<UnknownFailure, ToDoTask> call(int id) {
+    final result = repository.getTask(id);
+    if (result.isRight()) {
+      return Right(result);
+    } else {
+      return Left(UnknownFailure());
+    }
   }
 }
