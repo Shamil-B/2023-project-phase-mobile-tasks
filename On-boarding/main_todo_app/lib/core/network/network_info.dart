63a1be4935c83d197bd84e6e3b7df728c5c547e1
Shamil <shamilbedru47@gmail.com>
2023-08-11 14:25:50 +0300

    Update network_info.dart

--- On-boarding/main_todo_app/lib/core/network/network_info.dart
+++ On-boarding/main_todo_app/lib/core/network/network_info.dart
@@ -1,15 +1,14 @@
 import 'package:connectivity/connectivity.dart';
 
-class NetworkInfo {
+abstract class NetworkInfo{
+  Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl {
   final Connectivity _connectivity = Connectivity();
 
   Future<bool> get isConnected async {
     final result = await _connectivity.checkConnectivity();
     return result != ConnectivityResult.none;
-  }
-
-  Stream<bool> get onConnectionChange {
-    return _connectivity.onConnectivityChanged
-        .map((result) => result != ConnectivityResult.none);
   }
 }
